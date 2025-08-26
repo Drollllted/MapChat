36ab770fb50815bd911e7784774b8774df11a2fb
Artem Tarasov <artemdrolllted@gmail.com>
2025-08-26 12:51:51 +0300

    Initial Commit

---
+++ Fit/FitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitApp.swift
+//  Fit
+//
+//  Created by Drolllted on 26.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct FitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
